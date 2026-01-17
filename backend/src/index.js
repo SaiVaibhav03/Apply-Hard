14e292e9ef2a095a43aba9b3c25979d7973caf68
SaiVaibhav03 <vaibhavbalusa@gmail.com>
2026-01-17 19:18:26 +0530

    chore(backend): dotenv Setup and configure Environment variables on CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,9 @@
 const app = require('./app');
 
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+	require('dotenv').config();
+}
+
 const server = app.listen(3000, () => {
 	console.log('3000 port listening');
 });
