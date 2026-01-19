cdcffec54e7c1498242f476d27815b99811aecde
SaiVaibhav03 <vaibhavbalusa@gmail.com>
2026-01-19 20:46:07 +0530

    chore(backend-index.js): start database, start listening server, gracefull shutdown & handled uncaughtException and unhandledRejection Errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,39 @@
-const app = require('./app');
+require('dotenv').config();
 
-if (process.env.NODE_ENV !== 'PRODUCTION') {
-	require('dotenv').config();
+const app = require('./app');
+const { connectDatabase, disconnectDatabase } = require('./config/database');
+
+process.on('uncaughtException', (err) => {
+	console.log(`Error: ${err}`);
+	console.log(`Shutting down the server due to Uncaught Exception`);
+
+	process.exit(1); // Exit with an error code
+});
+
+connectDatabase();
+
+// cloudinary
+
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, () => {
+	console.log(`Server running on PORT ${PORT}`);
+});
+
+process.on('unhandledRejection', (err) => {
+	console.log(`Error: ${err}`);
+	shutdown(1, 'Unhandled Promise Rejection');
+});
+
+function shutdown(code = 0, signal) {
+	server.close(() => {
+		console.log(`Shutting down the server due to ${signal}`);
+		disconnectDatabase().finally(() => process.exit(code));
+	});
+
+	setTimeout(() => {
+		process.exit(1);
+	}, 10000); // 10sec
 }
 
-const server = app.listen(3000, () => {
-	console.log('3000 port listening');
-});
+process.on('SIGINT', () => shutdown(0, 'SIGINT signal from terminal')); // signal comes from terminal - ctrl + c
+process.on('SIGTERM', () => shutdown(0, 'SIGTERM signal from os/docker')); // signal comes from os/docker
